5bdcbb6f44e0acb39de87cd90882224a052d1c07
SABARISH <sabarish@SABARISHs-MacBook-Pro.local>
2023-10-29 14:41:22 +0530

    added links and redirect urls

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ExperienceData } from '@/Database/ExperienceData'
+import Link from 'next/link'
 
 export default function Experience() {
   return (
@@ -14,7 +15,10 @@ export default function Experience() {
                 
                 <div className='border m-1 md:w-[600px] md:max-h-[300px] md:m-5 rounded-[10px] shadow-md hover:border-lime-500 overflow-auto' key={experience_data.id}>
                     <div className='flex'>
-                        <p className='pt-3 pl-3 font-semibold text-lg md:text-xl'>{experience_data.position} . {experience_data.organization} </p><span className='pt-3 pl-1 font-bold cursor-pointer mt-1'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="darkgrey" fillRule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clipRule="evenodd"/></svg></span>
+                        <Link href={experience_data.link}>
+                        <p className='pt-3 pl-3 font-semibold text-lg md:text-xl'>{experience_data.position} . {experience_data.organization} </p>
+                        </Link>
+                        <span className='pt-3 pl-1 font-bold cursor-pointer mt-1'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="darkgrey" fillRule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clipRule="evenodd"/></svg></span>
                         <div className='ml-auto m-1 mt-3 rounded-lg bg-lime-500 hidden md:block'>
                             <p className='pl-1 pr-1 text-sm md:text-md'>{experience_data.duration}</p>
                         </div>
--- src/Database/ProjectData.ts
+++ src/Database/ProjectData.ts
@@ -7,8 +7,8 @@ export const ProjectData = [
         Description : "The objective of the project is to get the Global stock Streaming data via Google sheets and sending it to Website through Google API’s once data received it is processed by Python libraries and the end result is shown to the user.",
         Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','https://cdn-icons-png.flaticon.com/512/888/888859.png','https://cdn-icons-png.flaticon.com/512/732/732190.png','https://cdn-icons-png.flaticon.com/512/732/732204.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png'],
         image : "https://t4.ftcdn.net/jpg/02/70/95/21/360_F_270952103_2zSDVMWHM7KFOXmO0Dko0pYOE9aCs07k.jpg",
-        github : '',
-        live_link : ''
+        github : 'https://github.com/sunshine-22/stockanalysis.git',
+        live_link : 'https://github.com/sunshine-22/stockanalysis.git'
 
     },
     {
@@ -17,8 +17,8 @@ export const ProjectData = [
         Description : "Hello-chat is an online chat portal, The main objective of this project is to establish a conversation between a group of users. The Concept used in this project is to authenticate the new users and make the users to create encrypted chat rooms for communication.",
         Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/800px-HTML5_Badge.svg.png','https://seeklogo.com/images/S/socketio-logo-B8A7F486CD-seeklogo.com.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png','https://cdn-icons-png.flaticon.com/512/5968/5968292.png','https://miro.medium.com/v2/resize:fit:327/1*oLvPAqketvcwKRALKcFXyQ.png'],
         image : "https://img.freepik.com/premium-vector/simple-ui-design-chat-mobile-app_490632-177.jpg?w=2000",
-        github : '',
-        live_link : ''
+        github : 'https://github.com/sunshine-22/wechat.git',
+        live_link : 'https://github.com/sunshine-22/wechat.git'
     },
     {
         id : "3",
@@ -26,8 +26,8 @@ export const ProjectData = [
         Description : "Petzonic is an online pet selling android application, where a user can post the pet details and finds a client to purchase the pets with over 200+ available active pet seller community",
         Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="128" height="128" viewBox="0 0 128 128"%3E%3Cpath fill="%2338bdf8" d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597c6.398-8.531 13.867-11.73 22.398-9.597c4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602c-6.399 8.536-13.867 11.735-22.399 9.602c-4.87-1.215-8.347-4.746-12.207-8.66c-6.27-6.367-13.53-13.738-29.394-13.738zM32.004 64c-17.066 0-27.73 8.531-32 25.602C6.402 81.066 13.87 77.867 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66c6.274 6.367 13.536 13.738 29.395 13.738c17.066 0 27.73-8.53 32-25.597c-6.399 8.531-13.867 11.73-22.399 9.597c-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64zm0 0"%2F%3E%3C%2Fsvg%3E','https://cdn-icons-png.flaticon.com/512/919/919851.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png','https://cdn-icons-png.flaticon.com/512/5968/5968292.png','https://miro.medium.com/v2/resize:fit:327/1*oLvPAqketvcwKRALKcFXyQ.png','https://cdn-icons-png.flaticon.com/512/5968/5968342.png','https://w7.pngwing.com/pngs/246/288/png-transparent-firebase-hd-logo-thumbnail.png'],
         image : "https://cdn.dribbble.com/users/5187829/screenshots/17163068/media/f1bc154c2ef3b1fee862b8ee51891a78.jpg?resize=1600x1200&vertical=center",
-        github : '',
-        live_link : ''
+        github : 'https://github.com/sunshine-22/petzonic.git',
+        live_link : 'play.google.com'
     },
     {
         id : "4",
@@ -35,8 +35,8 @@ export const ProjectData = [
         Description : "TripsyGo is an online web portal to find all the travel spots around you and a one stop platform to meet with all your travel requirements.",
         Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="128" height="128" viewBox="0 0 128 128"%3E%3Cpath fill="%2338bdf8" d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597c6.398-8.531 13.867-11.73 22.398-9.597c4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602c-6.399 8.536-13.867 11.735-22.399 9.602c-4.87-1.215-8.347-4.746-12.207-8.66c-6.27-6.367-13.53-13.738-29.394-13.738zM32.004 64c-17.066 0-27.73 8.531-32 25.602C6.402 81.066 13.87 77.867 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66c6.274 6.367 13.536 13.738 29.395 13.738c17.066 0 27.73-8.53 32-25.597c-6.399 8.531-13.867 11.73-22.399 9.597c-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64zm0 0"%2F%3E%3C%2Fsvg%3E','https://cdn-icons-png.flaticon.com/512/919/919851.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png','https://cdn-icons-png.flaticon.com/512/5968/5968292.png','https://miro.medium.com/v2/resize:fit:327/1*oLvPAqketvcwKRALKcFXyQ.png','https://static-00.iconduck.com/assets.00/aws-icon-2048x2048-274bm1xi.png','https://cdn-icons-png.flaticon.com/512/5968/5968342.png'],
         image : "https://cdn.dribbble.com/users/5554868/screenshots/15838680/media/e0d8cad6db605f355839bac53e76887b.jpg?resize=1600x1200&vertical=center",
-        github : '',
-        live_link : ''
+        github : 'https://github.com/sunshine-22',
+        live_link : 'www.tripsygo.com'
     },
     {
         id : "5",
@@ -44,7 +44,16 @@ export const ProjectData = [
         Description : "OrgaPlants is an online E-Commerce Platform to buy and sell the Organic Products and manage your custom inventry and Dashboards and live tracking for Delivery location.",
         Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="128" height="128" viewBox="0 0 128 128"%3E%3Cpath fill="%2338bdf8" d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597c6.398-8.531 13.867-11.73 22.398-9.597c4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602c-6.399 8.536-13.867 11.735-22.399 9.602c-4.87-1.215-8.347-4.746-12.207-8.66c-6.27-6.367-13.53-13.738-29.394-13.738zM32.004 64c-17.066 0-27.73 8.531-32 25.602C6.402 81.066 13.87 77.867 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66c6.274 6.367 13.536 13.738 29.395 13.738c17.066 0 27.73-8.53 32-25.597c-6.399 8.531-13.867 11.73-22.399 9.597c-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64zm0 0"%2F%3E%3C%2Fsvg%3E','https://cdn-icons-png.flaticon.com/512/919/919851.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png','https://cdn-icons-png.flaticon.com/512/5968/5968292.png','https://miro.medium.com/v2/resize:fit:327/1*oLvPAqketvcwKRALKcFXyQ.png','https://static-00.iconduck.com/assets.00/aws-icon-2048x2048-274bm1xi.png','https://cdn-icons-png.flaticon.com/512/5968/5968322.png','https://cdn.iconscout.com/icon/free/png-256/free-mongodb-4-1175139.png','https://cdn-icons-png.flaticon.com/512/5968/5968342.png'],
         image : "https://cdn.dribbble.com/users/1463026/screenshots/13941613/media/7a8de83cfd329f3bf637ba0047aa7cda.png?resize=1600x1200&vertical=center",
-        github : '',
-        live_link : ''
+        github : 'https://github.com/sunshine-22',
+        live_link : 'https://orgaplants.com'
+    },
+    {
+        id : "6",
+        title : "MightyMovies",
+        Description : "MightyMovies is an online movie Recommendation platform where user's can search and watch the movie trailer's and comment their thoughts about the movie and rate movies.",
+        Stacks : ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png','data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="128" height="128" viewBox="0 0 128 128"%3E%3Cpath fill="%2338bdf8" d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597c6.398-8.531 13.867-11.73 22.398-9.597c4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602c-6.399 8.536-13.867 11.735-22.399 9.602c-4.87-1.215-8.347-4.746-12.207-8.66c-6.27-6.367-13.53-13.738-29.394-13.738zM32.004 64c-17.066 0-27.73 8.531-32 25.602C6.402 81.066 13.87 77.867 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66c6.274 6.367 13.536 13.738 29.395 13.738c17.066 0 27.73-8.53 32-25.597c-6.399 8.531-13.867 11.73-22.399 9.597c-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64zm0 0"%2F%3E%3C%2Fsvg%3E','https://cdn-icons-png.flaticon.com/512/919/919851.png','https://icons.veryicon.com/png/o/business/vscode-program-item-icon/django-1.png','https://cdn-icons-png.flaticon.com/512/5968/5968292.png','https://miro.medium.com/v2/resize:fit:327/1*oLvPAqketvcwKRALKcFXyQ.png','https://cdn-icons-png.flaticon.com/512/5968/5968342.png','data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="32" height="32" viewBox="0 0 32 32"%3E%3Cpath fill="gold" d="M19.078 12.786v.005c-.099-.063-.302-.094-.557-.094v6.422c.359 0 .583-.083.667-.224c.083-.135.125-.536.125-1.177v-3.823c0-.438-.005-.719-.042-.839c-.031-.13-.089-.219-.188-.271zM29.885 0H2.161A2.299 2.299 0 0 0 0 2.13v27.708c.078 1.167.948 2.057 2.073 2.156c.021.005.042.005.063.005h27.792a2.303 2.303 0 0 0 2.073-2.281V2.28a2.3 2.3 0 0 0-2.115-2.281zM6.391 20.833H3.849v-9.818h2.542zm8.718 0h-2.214v-6.63l-.896 6.625h-1.583l-.932-6.479l-.01 6.479H7.255v-9.813h3.286c.115.693.214 1.396.307 2.099l.359 2.49l.594-4.589h3.307zm6.636-2.906c0 .87-.057 1.458-.141 1.76a1.273 1.273 0 0 1-.432.693a1.632 1.632 0 0 1-.76.354c-.297.057-.76.099-1.359.099l-.005-.005h-3.073v-9.813h1.901c1.219 0 1.932.063 2.359.167c.432.12.766.302.995.563c.219.24.365.536.417.859c.068.313.099.938.099 1.87zm6.594.63c0 .599-.063 1.021-.12 1.323c-.083.297-.26.536-.542.755c-.302.224-.641.323-1.042.323c-.292 0-.667-.083-.906-.182a2.192 2.192 0 0 1-.688-.573l-.151.63h-2.292v-9.818l-.026-.005h2.401v3.198c.198-.234.422-.411.677-.531a2.62 2.62 0 0 1 .922-.172c.302 0 .599.047.88.156c.229.094.427.245.583.438c.12.167.198.359.24.563c.036.182.057.573.057 1.156v2.74zm-2.901-3.619c-.156 0-.255.057-.297.161c-.042.109-.078.385-.078.833v2.594c0 .432.036.714.078.833a.312.312 0 0 0 .302.177c.156 0 .359-.063.401-.188c.036-.13.057-.427.057-.896l.042-.005v-2.521c0-.401-.021-.677-.078-.802c-.063-.135-.26-.188-.422-.188z"%2F%3E%3C%2Fsvg%3E'],
+        image : "https://cdn.dribbble.com/userupload/4603456/file/original-16912cff12d344f59f248346bafaaf7d.jpg?resize=2048x1537",
+        github : 'https://github.com/sunshine-22/mightymovies.git',
+        live_link : 'https://github.com/sunshine-22/mightymovies.git'
     }
 ]
